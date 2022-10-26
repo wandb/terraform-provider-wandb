5e17021b60c993060fa76df5cf22bffdb5d7372a
Ishita Bindlish <ishita.bindlish@wandb.com>
2022-10-26 11:11:09 -0700

    create team query

--- wandb/provider/client.go
+++ wandb/provider/client.go
@@ -1,4 +1,4 @@
-package provider
+package main
 
 import (
 	"fmt"
@@ -138,32 +138,32 @@ func (c *Client) CreateTeam(method string, endpoint string, api_key string) (err
 	if orgResult.OrgData.Org.Available == false {
 		fmt.Println("organization doesn't have any teams left")
 	}
-	fmt.Println(string(body))
-	fmt.Println(orgResult.OrgData.Org)
-	fmt.Println(string(orgResult.OrgData.Org.ID))
 
 	// Create Team
 	// TODO: input arguments for mutation
 	teamName := "tmp-team2"
 	organizationId := orgResult.OrgData.Org.ID
 	jsonData := map[string]string{
-        "mutation": fmt.Sprintf(`
-            { 
-                createTeam (teamName: "%s", organizationId: "%s"){
-                    entity{
+        "query": fmt.Sprintf(` 
+			mutation {
+				createTeam (
+					input: {
+						teamName: "%s"
+						organizationId: "%s"
+					}
+				){
+					entity{
 						id
 						name
 					}
-                }
-            }
+				}
+			}
         `,
 		teamName,
 		organizationId,
 	),
     }
 	jsonValue, _ = json.Marshal(jsonData)
-
-	fmt.Println(string(jsonValue))
 	resp, err = c.doQuery(method, endpoint, api_key, bytes.NewBuffer(jsonValue))
 
 	if err != nil{
@@ -197,15 +197,15 @@ func (c *Client) CreateTeam(method string, endpoint string, api_key string) (err
 }
 
 
-// func main() {
-// 	defaultTimeout := time.Second * 10
-// 	client := NewClient("https://api.wandb.ai", "19f7df3fa4db872d5e4cea31ed8076e6b1ff5913", defaultTimeout)
+func main() {
+	defaultTimeout := time.Second * 10
+	client := NewClient("https://api.wandb.ai", "19f7df3fa4db872d5e4cea31ed8076e6b1ff5913", defaultTimeout)
 
-// 	host := "https://api.wandb.ai"
-// 	err := client.CreateTeam("POST", host + "/graphql", "19f7df3fa4db872d5e4cea31ed8076e6b1ff5913")
-// 	if err != nil{
-// 		fmt.Println(err)
-// 	}
-// }
+	host := "https://api.wandb.ai"
+	err := client.CreateTeam("POST", host + "/graphql", "19f7df3fa4db872d5e4cea31ed8076e6b1ff5913")
+	if err != nil{
+		fmt.Println(err)
+	}
+}
 
 
